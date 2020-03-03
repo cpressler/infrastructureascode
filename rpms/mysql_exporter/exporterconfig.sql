aae5ba48a40cc207f7705a4a33ab562d658c702a
Chester Pressler <chester.pressler@softvision.com>
2020-03-03 14:22:21 -0800

    some updates and added filebeat. Also added a playbook to install docker via ansible

--- rpms/mysql_exporter/exporterconfig.sql
+++ rpms/mysql_exporter/exporterconfig.sql
@@ -2,6 +2,6 @@
 --- only set low in lower environment
 -- must match settings in .mysql_exporter.cnf
 SET GLOBAL validate_password_policy=LOW;
-CREATE USER 'prom'@'localhost' identified by 'prometheus';
-GRANT REPLICATION CLIENT, PROCESS ON *.* TO 'prom'@'localhost';
+CREATE USER 'prometheus'@'localhost' identified by 'prometheus';
+GRANT REPLICATION CLIENT, PROCESS ON *.* TO 'prometheus'@'localhost';
 flush privileges;
